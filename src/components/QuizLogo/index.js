ba14cf37250c1f517c8bd561de79fa80807a0b19
Leonardo Tavares <leonardotg07@hotmail.com>
2021-01-26 21:46:09 -0300

    update logo

--- src/components/QuizLogo/index.js
+++ src/components/QuizLogo/index.js
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 
 function Logo({ className }) {
   return (
-    <svg className={className} width="320" height="136" viewBox="0 0 320 136" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="320" height="136" fill="url(#pattern0)" />
+    <svg className={className} width="280" height="119" viewBox="0 0 280 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="280" height="119" fill="url(#pattern0)" />
       <defs>
         <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
           <use xlinkHref="#image0" transform="scale(0.003125 0.00735294)" />
